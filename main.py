104a4d0d956548dd30315eb306e55c0cb0e3d011
vinaymancha <34467294+vinaymancha@users.noreply.github.com>
2020-03-21 19:52:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import sys
 
 # Global variables
 BATCH_SIZE = 64
-EPOCH = 200
+EPOCH = 100
 VAL_FREQ = 5
 NET_ARCH = 'DNN'
 
@@ -51,11 +51,11 @@ def RMSE_np(x, y):
     return np.sqrt(np.sum(np.square(x - y)) / n)
   
 if __name__ == "__main__":
-        for dataset_name in dataset_names:
+    for dataset_name in dataset_names:
         test_stat_hold = list()
         best_RMSE = float("inf")
 
-        print 'Training on Data-set: ' + dataset_name
+        print('Training on Data-set: ' + dataset_name)
         test_file = data_root + dataset_name + '_test_disguised.csv'
         train_file = data_root + dataset_name + '_training_disguised.csv'
 
@@ -67,12 +67,9 @@ if __name__ == "__main__":
         data_train, data_val = data_train >> SplitRandom(ratio=0.8) >> Collect()
         data_test = ReadPandas(test_file, dropnan=True)
 
+
         def organize_features(sample):
-            """
-            reorganize the flow as a feature vector predictor pair
-            :param sample: A row of data comming through the pipe
-            :return: a tupe consising feature vector and predictor
-            """
+           
             y = [sample[Act_inx], ]
             features = list(sample[Act_inx+1:])
             return (features, y)
@@ -84,8 +81,8 @@ if __name__ == "__main__":
         if NET_ARCH == 'deep_net':
             model = deep_net(input_shape=(feature_dim,))
             opti = Adam(lr=0.0001, beta_1=0.5)
-        elif NET_ARCH == 'merck_net':
-            model = merck_net(input_shape=(feature_dim,))
+        elif NET_ARCH == 'DNN':
+            model = DNN(input_shape=(feature_dim,))
             opti = sgd(lr=0.05, momentum=0.9, clipnorm=1.0)
         else:
             sys.exit("Network not defined correctly, check NET_ARCH. ")
@@ -109,38 +106,33 @@ if __name__ == "__main__":
         trues = data_val >> GetCols(Act_inx) >> Map(scale_activators) >> Collect()
         for e in range(1, EPOCH + 1):
             # training the network
-            data_train >> Shuffle(1000) >> Map(organize_features) >> NOP(PrintColType()) >> build_batch >> Map(
-                train_network_batch) >> NOP(Print()) >> Consume()
+            data_train >> Shuffle(1000) >> Map(organize_features) >> NOP(PrintColType()) >> build_batch >> Map(train_network_batch) >> NOP(Print()) >> Consume()
 
             # test the network every VAL_FREQ iteration
             if int(e) % VAL_FREQ == 0:
-                preds = data_val >> Map(organize_features) >> build_batch >> Map(
-                    predict_network_batch) >> Flatten() >> Map(scale_activators) >> Collect()
+                preds = data_val >> Map(organize_features) >> build_batch >> Map(predict_network_batch) >> Flatten() >> Map(scale_activators) >> Collect()
 
                 RMSE_e = RMSE_np(preds, trues)
                 Rsquared_e = Rsqured_np(preds, trues)
-                print 'Dataset ' + dataset_name + ' Epoch ' + str(e), ' : RMSE = ' + str(
-                    RMSE_e) + ', R-Squared = ' + str(Rsquared_e)
+                print('Dataset ' + dataset_name + ' Epoch ' + str(e), ' : RMSE = ' + str(RMSE_e) + ', R-Squared = ' + str(Rsquared_e))
                 test_stat_hold.append(('Epoch ' + str(e), RMSE_e, Rsquared_e))
 
                 if RMSE_e < best_RMSE:
-                    model.save_weights('./outputs/weights_' + dataset_name + '.h5')
+                    model.save_weights('C:/Users/vinay/Desktop/Drug Discovery/outputs/weights_' + dataset_name + '.h5')
                     best_RMSE = RMSE_e
 
-        print "Calculating errors for test set ..."
-        model.load_weights('./outputs/weights_' + dataset_name + '.h5')
+        print("Calculating errors for test set ...")
+        model.load_weights('C:/Users/vinay/Desktop/Drug Discovery/outputs/weights_' + dataset_name + '.h5')
         trues = data_test >> GetCols(Act_inx) >> Map(scale_activators) >> Collect()
 
-        preds = data_test >> Map(organize_features) >> build_batch >> Map(predict_network_batch) >> Flatten() >> Map(
-            scale_activators) >> Collect()
+        preds = data_test >> Map(organize_features) >> build_batch >> Map(predict_network_batch) >> Flatten() >> Map(scale_activators) >> Collect()
 
         RMSE_e = RMSE_np(preds, trues)
         Rsquared_e = Rsqured_np(preds, trues)
-        print 'Dataset ' + dataset_name + ' Test : RMSE = ' + str(RMSE_e) + ', R-Squared = ' + str(Rsquared_e)
+        print('Dataset ' + dataset_name + ' Test : RMSE = ' + str(RMSE_e) + ', R-Squared = ' + str(Rsquared_e))
         test_stat_hold.append(('Final', RMSE_e, Rsquared_e))
 
-        writer = WriteCSV('./outputs/test_errors_' + dataset_name + '.csv')
+        writer = WriteCSV('C:/Users/vinay/Desktop/Drug Discovery/outputs/test_errors_' + dataset_name + '.csv')
         test_stat_hold >> writer
-  
   
   
