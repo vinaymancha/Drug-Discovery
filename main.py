ce713148feaa05591ca7a85fa2f8af506493b341
vinaymancha <34467294+vinaymancha@users.noreply.github.com>
2020-03-21 18:04:10 +0530

    Add files via upload
    update bro

--- main.py
+++ main.py
@@ -66,6 +66,81 @@ if __name__ == "__main__":
         # split randomly train and val
         data_train, data_val = data_train >> SplitRandom(ratio=0.8) >> Collect()
         data_test = ReadPandas(test_file, dropnan=True)
+
+        def organize_features(sample):
+            """
+            reorganize the flow as a feature vector predictor pair
+            :param sample: A row of data comming through the pipe
+            :return: a tupe consising feature vector and predictor
+            """
+            y = [sample[Act_inx], ]
+            features = list(sample[Act_inx+1:])
+            return (features, y)
+
+        build_batch = (BuildBatch(BATCH_SIZE)
+                       .by(0, 'vector', float)
+                       .by(1, 'number', float))
+
+        if NET_ARCH == 'deep_net':
+            model = deep_net(input_shape=(feature_dim,))
+            opti = Adam(lr=0.0001, beta_1=0.5)
+        elif NET_ARCH == 'merck_net':
+            model = merck_net(input_shape=(feature_dim,))
+            opti = sgd(lr=0.05, momentum=0.9, clipnorm=1.0)
+        else:
+            sys.exit("Network not defined correctly, check NET_ARCH. ")
+
+        model.compile(optimizer=opti, loss='mean_squared_error', metrics=[Rsqured])
+
+        def train_network_batch(sample):
+            tloss = model.train_on_batch(sample[0], sample[1])
+            return (tloss[0], tloss[1])
+
+        def test_network_batch(sample):
+            tloss = model.test_on_batch(sample[0], sample[1])
+            return (tloss[0],)
+
+        def predict_network_batch(sample):
+            return model.predict(sample[0])
+
+        scale_activators = lambda x: (
+            x[0] * dataset_stats.loc[dataset_name, 'std'] + dataset_stats.loc[dataset_name, 'mean'])
+
+        trues = data_val >> GetCols(Act_inx) >> Map(scale_activators) >> Collect()
+        for e in range(1, EPOCH + 1):
+            # training the network
+            data_train >> Shuffle(1000) >> Map(organize_features) >> NOP(PrintColType()) >> build_batch >> Map(
+                train_network_batch) >> NOP(Print()) >> Consume()
+
+            # test the network every VAL_FREQ iteration
+            if int(e) % VAL_FREQ == 0:
+                preds = data_val >> Map(organize_features) >> build_batch >> Map(
+                    predict_network_batch) >> Flatten() >> Map(scale_activators) >> Collect()
+
+                RMSE_e = RMSE_np(preds, trues)
+                Rsquared_e = Rsqured_np(preds, trues)
+                print 'Dataset ' + dataset_name + ' Epoch ' + str(e), ' : RMSE = ' + str(
+                    RMSE_e) + ', R-Squared = ' + str(Rsquared_e)
+                test_stat_hold.append(('Epoch ' + str(e), RMSE_e, Rsquared_e))
+
+                if RMSE_e < best_RMSE:
+                    model.save_weights('./outputs/weights_' + dataset_name + '.h5')
+                    best_RMSE = RMSE_e
+
+        print "Calculating errors for test set ..."
+        model.load_weights('./outputs/weights_' + dataset_name + '.h5')
+        trues = data_test >> GetCols(Act_inx) >> Map(scale_activators) >> Collect()
+
+        preds = data_test >> Map(organize_features) >> build_batch >> Map(predict_network_batch) >> Flatten() >> Map(
+            scale_activators) >> Collect()
+
+        RMSE_e = RMSE_np(preds, trues)
+        Rsquared_e = Rsqured_np(preds, trues)
+        print 'Dataset ' + dataset_name + ' Test : RMSE = ' + str(RMSE_e) + ', R-Squared = ' + str(Rsquared_e)
+        test_stat_hold.append(('Final', RMSE_e, Rsquared_e))
+
+        writer = WriteCSV('./outputs/test_errors_' + dataset_name + '.csv')
+        test_stat_hold >> writer
   
   
   
