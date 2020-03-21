252b42ec9396d61f4360ee002f57504b1f3a361a
vinaymancha <34467294+vinaymancha@users.noreply.github.com>
2020-03-22 00:38:29 +0530

    Parameter-Tuning

--- model.py
+++ model.py
@@ -14,10 +14,10 @@ def DNN(input_shape=(128)):
     model.add(Dense(512, activation='relu', input_shape=input_shape, kernel_regularizer=l2(0.0001)))
     model.add(Dropout(0.25))
 
-    model.add(Dense(512, activation='relu', kernel_regularizer=l2(0.0001)))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.0001)))
     model.add(Dropout(0.25))
 
-    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.0001)))
+    model.add(Dense(128, activation='relu', kernel_regularizer=l2(0.0001)))
     model.add(Dropout(0.10))
 
     model.add(Dense(1, activation=None, use_bias=True, kernel_regularizer=l2(0.0001)))
